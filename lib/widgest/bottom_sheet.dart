0c7726b6f97b9669a1e80f33a70b0923b39013ed
1Shibam <hehe.shibam@gmail.com>
2024-12-24 18:42:10 +0530

    bottom sheet widget

--- lib/widgest/bottom_sheet.dart
+++ lib/widgest/bottom_sheet.dart
@@ -10,6 +10,6 @@ class BottomSheetWidget extends StatefulWidget {
 class _BottomSheetWidgetState extends State<BottomSheetWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
