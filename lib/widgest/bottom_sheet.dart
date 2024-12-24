8d46c9b785ed01de95463bbb419f1f9ed1854e85
1Shibam <hehe.shibam@gmail.com>
2024-12-24 19:21:46 +0530

    add bottom sheet widget

--- lib/widgest/bottom_sheet.dart
+++ lib/widgest/bottom_sheet.dart
@@ -10,6 +10,56 @@ class BottomSheetWidget extends StatefulWidget {
 class _BottomSheetWidgetState extends State<BottomSheetWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                elevation: 0,
+                isDismissible: false,
+                showDragHandle: true,
+                backgroundColor: Colors.blue,
+                sheetAnimationStyle: AnimationStyle(
+                    curve: Easing.emphasizedDecelerate,
+                    duration: const Duration(milliseconds: 600)),
+                enableDrag: true,
+                barrierColor: Colors.grey,
+                context: context,
+                builder: (context) {
+                  return const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-1'),
+                      ),
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-2'),
+                      ),
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-3'),
+                      ),
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-4'),
+                      ),
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-5'),
+                      ),
+                      ListTile(
+                        title: Text('niggers'),
+                        subtitle: Text('Nigga-6'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Text('Show Bottom Sheet')),
+      ),
+    );
   }
 }
