8e0fb2e29d3ef30e8400325d92a667b723963265
1Shibam <hehe.shibam@gmail.com>
2024-12-25 18:03:53 +0530

    complete stack widget

--- lib/widgest/stack_widget.dart
+++ lib/widgest/stack_widget.dart
@@ -5,6 +5,31 @@ class StackWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.blue, Colors.deepPurple],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 30,
+              ),
+              Container(
+                color: Colors.green,
+                height: 30,
+                width: 100,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
