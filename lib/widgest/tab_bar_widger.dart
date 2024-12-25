565e5a0796ed9ea4b170ce2c415add1c1cfa0a3e
1Shibam <hehe.shibam@gmail.com>
2024-12-25 18:59:03 +0530

    complete Tab bar widget

--- lib/widgest/tab_bar_widger.dart
+++ lib/widgest/tab_bar_widger.dart
@@ -5,6 +5,40 @@ class TabBarWidger extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('VibeTunes'),
+            bottom:
+                const TabBar(automaticIndicatorColorAdjustment: true,
+
+                
+                
+                
+                 tabs: [
+              Tab(
+                icon: Icon(Icons.music_note),
+                text: 'Songs',
+              ),
+              Tab(
+                icon: Icon(Icons.featured_play_list),
+                text: 'Playlists',
+              ),
+            ]),
+          ),
+          body: TabBarView(children: [
+            Container(
+              child: Center(
+                child: Text('Songs will be here!!'),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Text('Playlists will be here!!'),
+              ),
+            )
+          ]),
+        ));
   }
 }
