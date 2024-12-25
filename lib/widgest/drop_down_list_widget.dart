fe6e2d2ac2edb97ef229d65676c2e087e70a2302
1Shibam <hehe.shibam@gmail.com>
2024-12-25 11:07:45 +0530

    complete drop down list widget

--- lib/widgest/drop_down_list_widget.dart
+++ lib/widgest/drop_down_list_widget.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class DropDownListWidget extends ConsumerWidget {
   const DropDownListWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ExpansionTile(
+              
+              visualDensity: VisualDensity(),
+              initiallyExpanded: true,
+              title: Text(
+                'hello',
+              ),
+              childrenPadding: EdgeInsets.all(4),
+              collapsedBackgroundColor: Colors.transparent,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('hello'),
+                            Text('hello'),
+                            Text('hello'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
